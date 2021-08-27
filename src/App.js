5d849cc5a3e208024359643ea7e6a2bfd4660999
Fotis Skoulas <fotis_skoulas@hotmail.com>
2021-08-27 20:19:19 +0300

    Created app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react' ;
 import './App.css';
+
+function Header () {
+  return (
+  <header>
+    <h1> Eve Kitchen </h1>
+  </header>
+    );
+}
+function Main () {
+  return (
+    <section>
+      <p> We are all bill masters</p>
+    </section>
+  )
+}
+function Footer () {
+  return (
+    <footer>
+      <p>YEEEEEES</p>
+    </footer>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <Main />
+     <Footer />
     </div>
   );
 }
